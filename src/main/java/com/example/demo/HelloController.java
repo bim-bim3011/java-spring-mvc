4e2299d2aac7333ded0e9c947f4130d93b8d1123
Khanhnd <dangkhanh1130@gmail.com>
2025-12-29 16:38:58 +0700

    bai42

--- src/main/java/com/example/demo/HelloController.java
+++ src/main/java/com/example/demo/HelloController.java
@@ -7,9 +7,18 @@ import org.springframework.web.bind.annotation.GetMapping;
 public class HelloController {
 
     @GetMapping("/")
-
     public String index() {
-        return "hello world nguyen dang khanh ";
+        return "hello world ";
+    }
+
+    @GetMapping("/user")
+    public String userPage() {
+        return "ONly user can access this page";
+    }
+
+    @GetMapping("/admin")
+    public String adminPage() {
+        return "Only Admin";
     }
 
 }
